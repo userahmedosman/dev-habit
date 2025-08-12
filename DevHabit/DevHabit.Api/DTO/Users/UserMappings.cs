9586f3d0f352be62819550772c69ca37087ff78c
Hajo <ahmedhaj000@gmail.com>
2025-08-12 17:10:13 +0300

    Added first phase User Registration process

---
+++ DevHabit/DevHabit.Api/DTO/Users/UserMappings.cs
@@ -0,0 +1,19 @@
+﻿using DevHabit.Api.DTO.Auth;
+using DevHabit.Api.Entities;
+
+namespace DevHabit.Api.DTO.Users;
+
+public static class UserMappings
+{
+
+    public static User ToEntity(this RegisterUserDto registerUserDto)
+    {
+        return new User
+        {
+            Id = $"u_{Guid.CreateVersion7()}",
+            Name = registerUserDto.Name,
+            Email = registerUserDto.Email,
+            CreatedAtUtc = DateTime.UtcNow,
+        };
+    }
+}
